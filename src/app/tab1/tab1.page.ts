f927e1c8821afb1fa0cd2979cae66e03891b17c9
luizclaudio83 <lcb_83@hotmail.com>
2021-05-27 20:07:04 -0300

    Adicionamos array de filmes e a interfaces de filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,3 +1,4 @@
+import { IFilme } from '../models/ifilme.model';
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
 import { ToastController } from '@ionic/angular';
@@ -9,6 +10,38 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+
+  titulo = 'Videos';
+
+  listaVideos: IFilme[] = [
+    {
+       nome: 'Mortal Kombat (2021)',
+       lancamento: '15/04/2021',
+       duracao: '1h 50m',
+       classificacao: 56,
+       cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
+       generos: ['Ação', 'Fantasia', 'Aventura']
+    },
+    {
+       nome: 'Rogai Por Nós (2021)',
+       lancamento: '02/04/2021',
+       duracao: '1h 39m',
+       classificacao: 76,
+       cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/zduyzcjyahZtP4fGZE6789h04IM.jpg',
+       generos: ['Terror']
+    },
+    {
+      nome: 'Na Mira do Perigo (2021)',
+      lancamento: '18/02/2021',
+      duracao: '1h 48m',
+      classificacao: 74,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/wAJniojhHa4oVKa9AxdNwvpmS74.jpg',
+      generos: ['Ação', 'Thriller', 'Crime']
+   },
+
+  ];
+
+
 
   constructor(public alertController: AlertController, public toastController: ToastController) {}
 
